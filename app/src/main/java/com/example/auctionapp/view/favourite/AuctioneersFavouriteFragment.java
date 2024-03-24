273df33a56c259d03a1651c5052511c88eaf486a
DungD7 <dvdung2002@gmail.com>
2024-03-24 16:24:18 +0700

    commit 24/3 laptop

--- app/src/main/java/com/example/auctionapp/view/favourite/AuctioneersFavouriteFragment.java
+++ app/src/main/java/com/example/auctionapp/view/favourite/AuctioneersFavouriteFragment.java
@@ -1,6 +1,36 @@
 package com.example.auctionapp.view.favourite;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.auctionapp.databinding.FragmentAuctioneersFavouriteBinding;
+import com.example.auctionapp.databinding.FragmentItemsFavouriteBinding;
+
+
 public class AuctioneersFavouriteFragment extends Fragment {
+    FragmentAuctioneersFavouriteBinding binding;
+    public AuctioneersFavouriteFragment() {
+    }
+
+    public AuctioneersFavouriteFragment(int contentLayoutId) {
+        super(contentLayoutId);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentAuctioneersFavouriteBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
 }
